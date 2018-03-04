de7ea0ca92c4c72c80c645f7295ce53dbb3fd1fd
zhangchengtao <jokerwaver@gmail.com>
2018-03-04 20:46:55 +0800

    提交

---
+++ src/main/java/me/waver/gallery/service/PhotoService.java
@@ -0,0 +1,9 @@
+package me.waver.gallery.service;
+
+/**
+ * @author : Creeper
+ * @date : 2018/2/26 22:03
+ */
+public interface PhotoService {
+    public String addPhoto(String aublm);
+}
---
+++ src/main/java/me/waver/gallery/service/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package me.waver.gallery.service;
+
+/**
+ * @author : Creeper
+ * @date : 2018/2/26 22:02
+ */
+public class UserServiceImpl implements UserService{
+}
